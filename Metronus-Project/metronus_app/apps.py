b46170d90600d38b5a16ccf3349c759c2b06b1ec
agubelu <agustinbd3@gmail.com>
2017-02-25 12:54:22 +0100

    Initial version of Metronus Project

---
+++ Metronus-Project/metronus_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MetronusAppConfig(AppConfig):
+    name = 'metronus_app'
---
+++ Metronus-Project/metronus_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+import time
+
+# Create your views here.
+
+def index(request):
+	return render(request, "index.html", {"current_date": time.strftime("%Y-%m-%d %H:%M")})
+
