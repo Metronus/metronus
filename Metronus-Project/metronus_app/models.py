ebf6ddee72a1b0010470b35160eaba0a537954fd
Jose.A Rojo <josrojvid@alum.us.es>
2017-03-04 14:43:45 +0100

    Added Department to models

--- Metronus-Project/metronus_app/models.py
+++ Metronus-Project/metronus_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+	companyId 	= models.ForeignKey(Company)
+	name 		= models.CharField(max_length=30)
+	registryDate	= models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return self.name
+	
