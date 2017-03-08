e2da524c591e715d6ab4e759b94263dc7cea7ad4
agubelu <agustinbd3@gmail.com>
2017-03-08 18:59:36 +0100

    Link roles with their respective companies

--- Metronus-Project/metronus_app/model/role.py
+++ Metronus-Project/metronus_app/model/role.py
@@ -1,6 +1,10 @@
 from django.db import models
+from metronus_app.model.company import Company
 
 class Role(models.Model):
-    name=models.CharField(max_length=30)
+
+    name = models.CharField(max_length=50)
+    company_id = models.ForeignKey(Company)
+
     def __unicode__(self):
         return self.name
