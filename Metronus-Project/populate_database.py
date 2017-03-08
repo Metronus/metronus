b16408699179a037c6c85055fd316e75a80a4ab1
agubelu <agustinbd3@gmail.com>
2017-03-08 19:57:42 +0100

    merge load.py into populate_database

--- Metronus-Project/populate_database.py
+++ Metronus-Project/populate_database.py
@@ -1,4 +1,45 @@
 from metronus_app.model.role import Role
+from metronus_app.model.company import Company
+from metronus_app.model.employee import Employee
+from metronus_app.model.project import Project
+from metronus_app.model.department import Department
+from django.contrib.auth.models import User
+from metronus_app.model.administrator import Administrator
+
+
+def basicLoad():
+    Company.objects.create(cif="123", company_name="company1", short_name="company1", email="company1@gmail.com", phone="123456789")
+    company = Company.objects.get(cif="123")
+    User.objects.create_user(
+        username="employee",
+        password="employee",
+        email="employee@gmail.com",
+        first_name="employee",
+        last_name="employee")
+    user = User.objects.get(username="employee")
+    Employee.objects.create(
+        user=user,
+        user_type="E",
+        identifier="12345",
+        phone="123456789",
+        company_id=company)
+
+    User.objects.create_user(
+        username="admin",
+        password="admin",
+        email="admin@gmail.com",
+        first_name="admin",
+        last_name="admin")
+    admin = User.objects.get(username="admin")
+    Administrator.objects.create(
+        user=admin,
+        user_type="A",
+        identifier="12345",
+        phone="123456789",
+        company_id=company)
+    Department.objects.create(name="dep3",active=True,company_id=company)
+    Project.objects.create(name="TestProject",deleted=False,company_id=company)
+
 
 def populate_roles():
     print("==== POPULATING ROLES ====")
@@ -10,6 +51,10 @@ def populate_roles():
     Role.objects.create(name="Employee")
     print("==== %d ROLES INSERTED ====" % Role.objects.all().count())
 
+#############################################################################
+#############################################################################
+#############################################################################
 
 def populate_database():
+    basicLoad()
     populate_roles()
