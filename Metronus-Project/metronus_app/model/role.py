f04bb79855e0afd3d8131d1a9fc2a2511308b9df
agubelu <agustinbd3@gmail.com>
2017-03-08 19:32:21 +0100

    Set role names to be unique

--- Metronus-Project/metronus_app/model/role.py
+++ Metronus-Project/metronus_app/model/role.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Role(models.Model):
-    name=models.CharField(max_length=30)
+    name=models.CharField(max_length=30, unique=True)
+
     def __unicode__(self):
         return self.name
