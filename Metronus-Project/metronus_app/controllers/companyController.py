f51064e4e04ba9f3b662ca24e0509b72758c4701
Liebana <jcliebanafer@gmail.com>
2017-03-09 20:04:55 +0100

    Solución para añadir logo. (Ahora hay problemas en la validación)

--- Metronus-Project/metronus_app/controllers/companyController.py
+++ Metronus-Project/metronus_app/controllers/companyController.py
@@ -22,7 +22,7 @@ def create(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = CompanyRegistrationForm(request.POST)
+        form = CompanyRegistrationForm(request.POST, request.FILES)
         # check whether it's valid:
         if form.is_valid() and checkPasswords(form):
             # process the data in form.cleaned_data as required
