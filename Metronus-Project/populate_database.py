9776f5e60771ef241126b9ccb95f1109e4134a62
Andrés Doncel <andrestp95@gmail.com>
2017-03-09 15:29:32 +0100

    populate database con metronus registrado:nuestros users son los alias de telegram

--- Metronus-Project/populate_database.py
+++ Metronus-Project/populate_database.py
@@ -9,7 +9,10 @@ from metronus_app.model.administrator import Administrator
 
 def basicLoad():
     Company.objects.create(cif="123", company_name="company1", short_name="company1", email="company1@gmail.com", phone="123456789")
+    Company.objects.create(cif="000000000", company_name="metronus", short_name="met", email="metronus@us.com", phone="123456789")
     company = Company.objects.get(cif="123")
+    company2 = Company.objects.get(cif="000000000")
+    #From first company:company1
     User.objects.create_user(
         username="employee",
         password="employee",
@@ -37,9 +40,49 @@ def basicLoad():
         identifier="12345",
         phone="123456789",
         company_id=company)
+
+    #From second company:metronus
+    User.objects.create_user(
+        username="admin2",
+        password="admin2",
+        email="admin2@gmail.com",
+        first_name="admin2",
+        last_name="admin2")
+    admin2 = User.objects.get(username="admin2")
+    Administrator.objects.create(
+        user=admin2,
+        user_type="A",
+        identifier="12345",
+        phone="123456789",
+        company_id=company2)
+
+    create_user("anddonram",company2)
+    create_user("raisans_138",company2)
+    create_user("JoseGavilan",company2)
+    create_user("ddlsb",company2)
+    create_user("agubelu",company2)
+    create_user("JkLiebana",company2)
+    create_user("Guardsoul",company2)
+    create_user("Barrelito",company2)
+    create_user("andjimrio",company2)
+    create_user("Jarvie",company2)
     Department.objects.create(name="dep3",active=True,company_id=company)
     Project.objects.create(name="TestProject",deleted=False,company_id=company)
 
+def create_user(nombre,company2):
+    User.objects.create_user(
+        username=nombre,
+        password=nombre,
+        email=nombre+"@gmail.com",
+        first_name=nombre,
+        last_name=nombre)
+    user2 = User.objects.get(username=nombre)
+    Employee.objects.create(
+        user=user2,
+        user_type="E",
+        identifier="12345",
+        phone="123456789",
+        company_id=company2)
 
 def populate_roles():
     print("==== POPULATING ROLES ====")
