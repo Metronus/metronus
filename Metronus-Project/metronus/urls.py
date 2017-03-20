ab063934fc7edc78b86a18267a402fd8b0622e20
agubelu <agustinbd3@gmail.com>
2017-03-20 11:42:27 +0100

    Role deletion #9

--- Metronus-Project/metronus/urls.py
+++ Metronus-Project/metronus/urls.py
@@ -79,7 +79,9 @@ urlpatterns += [#i18n_patterns(
     url(r'^timeLog/edit/(?P<timeLog_id>\w{0,50})/$', timeLogController.edit, name='timeLog_edit'),
     url(r'^timeLog/delete/(?P<timeLog_id>\w{0,50})/$', timeLogController.delete, name='timeLog_delete'),
 
+    # Roles
     url(r'^roles/manage$', roleController.manage, name='roles_manage'),
+    url(r'^roles/delete/(?P<role_id>\d{0,50})/$', roleController.delete, name='roles_delete'),
 
     # Administrator
     url(r'^administrator/edit/(?P<username>\w{0,50})/$', administratorController.edit, name='administrator_edit'),
