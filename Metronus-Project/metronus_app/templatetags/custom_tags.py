34061b9b835d435f944b749f47af4e357268de4d
David de los Santos Boix <ddlsb@hotmail.com>
2017-03-21 11:34:25 +0100

    Añadida plantilla nueva

---
+++ Metronus-Project/metronus_app/templatetags/custom_tags.py
@@ -0,0 +1,21 @@
+from django import template
+
+register = template.Library()
+
+def get_type(ob):
+    return ob.field.widget.__class__.__name__
+
+def get_form_type(type):
+    return {
+        'TextInput' : 'text',
+        'PasswordInput' : 'password',
+        'EmailInput' : 'email'
+    }.get(type, 'text')
+
+@register.inclusion_tag('tags/form.html')
+def show_form(form):
+    return {'form':form}
+
+@register.inclusion_tag('tags/field.html')
+def show_field(field, required = True):
+    return {'field':field, 'required': "required" if required  else "", 'type':get_form_type(get_type(field))}
