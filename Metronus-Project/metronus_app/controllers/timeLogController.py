005a20ae91a05bc25e099da0e6d1845f394c897d
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2017-03-29 22:57:28 +0200

    UC-045: current month

--- Metronus-Project/metronus_app/controllers/timeLogController.py
+++ Metronus-Project/metronus_app/controllers/timeLogController.py
@@ -95,15 +95,15 @@ def list(request, task_id):
 def list_all(request):
     today = datetime.today()
     employee = get_current_employee_or_403(request)
-    if(request.GET.get('monthFilter')):
-        monthFilter = request.GET['monthFilter']
+    if(request.GET.get('currentMonth')):
+        currentMonth = request.GET['currentMonth']
     else:
-        monthFilter = today.month
+        currentMonth = today.month
 
-    if (request.GET.get('yearFilter')):
-        yearFilter = request.GET['yearFilter']
+    if (request.GET.get('currentYear')):
+        currentYear = request.GET['currentYear']
     else:
-        yearFilter = today.year
+        currentYear = today.year
 
     try:
         actor = Actor.objects.get(user=request.user)
@@ -115,7 +115,8 @@ def list_all(request):
         project = request.POST.get("project")
         department = request.POST.get("department")
 
-        if project=='': return
+        if project=='':
+            return
         else:
             print (project)
             print (department)
@@ -131,7 +132,8 @@ def list_all(request):
                 return HttpResponse(data)
             else:
                 print (department)
-                tasks = Task.objects.filter(projectDepartment_id__department_id=department,projectDepartment_id__project_id=project,active=True)
+                tasks = Task.objects.filter(projectDepartment_id__department_id=department,
+                                            projectDepartment_id__project_id=project,active=True)
                 data = serializers.serialize('json', tasks, fields=('id', 'name',))
 
                 return HttpResponse(data)
@@ -154,15 +156,14 @@ def list_all(request):
     tareas=Task.objects.filter(actor_id__company_id=actor.company_id,
                                    projectDepartment_id__projectdepartmentemployeerole__employee_id=actor,
                                    active=True).distinct()
-    my_tasks = [myTask(x,monthFilter,yearFilter) for x in tareas]
+    my_tasks = [myTask(x,currentMonth,currentYear) for x in tareas]
 
     month = [x for x in range(1,calendar.monthrange(today.year,today.month)[1]+1)]
     month.append("Total")
     total = [sum([x.durations[i] for x in my_tasks]) for i in range(0,calendar.monthrange(today.year,today.month)[1]) ]
     monthTotal = sum(total)
     total.append(monthTotal)
-    currentMonth = date.today().month
-    currentYear = date.today().year
+
     form = TimeLog2Form(request, initial={"timeLog_id": 0, "workDate": datetime.now()})
 
     return render(request, "timeLog/timeLog_list_all.html", {"my_tasks": my_tasks, "month":month,"total":total, "currentMonth":currentMonth, "currentYear":currentYear, "form":form})
