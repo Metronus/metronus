8c8f8e7dd7f8462c7ed27c8915347d44b5760edc
José Gavilán <indius10@hotmail.com>
2017-04-05 15:21:18 +0200

    Añadido el tag de errores de ajax

--- Metronus-Project/metronus_app/templatetags/custom_tags.py
+++ Metronus-Project/metronus_app/templatetags/custom_tags.py
@@ -21,7 +21,9 @@ def show_form(form):
 @register.inclusion_tag('tags/field.html')
 def show_field(field, required = True):
     return {'field':field, 'required': "required" if required  else "", 'type':get_form_type(get_type(field))}
-
+@register.inclusion_tag('tags/ajaxErrors.html')
+def show_ajax_errors():
+    return {}
 @register.simple_tag
 def converto_to_hours(amount):
     hours = amount//60
