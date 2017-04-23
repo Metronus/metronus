d70afde2a4548b1e21905574292153e0ad3f7b1c
raisans138 <jmjimenez95@gmail.com>
2017-04-24 00:07:39 +0200

    Refactorización de código en el controlador de Role

--- Metronus-Project/metronus/urls.py
+++ Metronus-Project/metronus/urls.py
@@ -108,7 +108,7 @@ urlpatterns += [  # i18n_patterns(
 
     # Roles
     url(r'^roles/manage$', roleController.manage, name='roles_manage'),
-    url(r'^roles/manageAsync$', roleController.manageAsync, name='roles_manageAsync'),
+    url(r'^roles/manageAsync$', roleController.manage_async, name='roles_manageAsync'),
     url(r'^roles/delete/(?P<role_id>\d{0,50})/$', roleController.delete, name='roles_delete'),
     url(r'^roles/availableDepartments', roleController.ajax_departments_from_projects, name='roles_dpmt_ajax'),
     url(r'^roles/availableRoles', roleController.ajax_roles_from_tuple, name='roles_role_ajax'),
