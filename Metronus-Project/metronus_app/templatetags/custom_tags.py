70086fe3a5214e52be0c5a8e1060e4fa6f3bd034
raisans138 <jmjimenez95@gmail.com>
2017-04-23 18:54:48 +0200

    Refactorización y limpieza de urls y custom tags

--- Metronus-Project/metronus_app/templatetags/custom_tags.py
+++ Metronus-Project/metronus_app/templatetags/custom_tags.py
@@ -1,63 +1,72 @@
 from django import template
 from datetime import datetime
 import calendar
-from django.core.exceptions import MultipleObjectsReturned
 from metronus_app.model.projectDepartmentEmployeeRole import ProjectDepartmentEmployeeRole
 
 register = template.Library()
 
+
 def get_type(ob):
     return ob.field.widget.__class__.__name__
 
-def get_form_type(type):
+
+def get_form_type(form_type):
     return {
-        'TextInput' : 'text',
-        'PasswordInput' : 'password',
-        'EmailInput' : 'email',
+        'TextInput': 'text',
+        'PasswordInput': 'password',
+        'EmailInput': 'email',
         'ClearableFileInput': 'file'
-    }.get(type, 'text')
+    }.get(form_type, 'text')
+
 
 @register.inclusion_tag('tags/form.html')
 def show_form(form):
-    return {'form':form}
+    return {'form': form}
+
 
 @register.inclusion_tag('tags/field.html')
-def show_field(field, required = True):
-    return {'field':field, 'required': "required" if required  else "", 'type':get_form_type(get_type(field))}
+def show_field(field, required=True):
+    return {'field': field, 'required': "required" if required else "", 'type': get_form_type(get_type(field))}
+
+
 @register.inclusion_tag('tags/ajaxErrors.html')
 def show_ajax_errors():
     return {}
+
+
 @register.simple_tag
 def converto_to_hours(amount):
     hours = amount//60
-    minutes = amount%60
-    minutos = ""
-    if(minutes<10):
+    minutes = amount % 60
+    if minutes < 10:
         minutos = "0"+str(minutes)
     else:
         minutos = str(minutes)
-    return str(hours)+":"+minutos if amount !=0 else ""
+    return str(hours)+":"+minutos if amount != 0 else ""
+
 
 @register.simple_tag
-def is_weekend(day,month,year):
+def is_weekend(day, month, year):
     result = True
-    if day != "Total" and int(day) != calendar.monthrange(year,month)[1]+1:
-        fecha = datetime(year,month,day).date()
-        result = fecha.weekday() in (5,6)
+    if day != "Total" and int(day) != calendar.monthrange(year, month)[1]+1:
+        fecha = datetime(year, month, day).date()
+        result = fecha.weekday() in (5, 6)
     return "success" if result else ""
 
+
 @register.assignment_tag
-def isAdmin(actor):
+def is_admin(actor):
     if actor.user_type == 'A':
         return True
     else:
         return False
 
+
 @register.assignment_tag
-def hasRole(actor):
+def has_role(actor):
     if actor.user_type == 'E' or actor.user_type == 'A':
         try:
-            if ProjectDepartmentEmployeeRole.objects.filter(employee_id = actor.id).count() > 0:
+            if ProjectDepartmentEmployeeRole.objects.filter(employee_id=actor.id).count() > 0:
                 return True
             else:
                 return False
@@ -68,35 +77,35 @@ def hasRole(actor):
 
 
 @register.assignment_tag
-def isEmployee(actor):
-    if actor.user_type == 'A' or not hasRole(actor):
+def is_employee(actor):
+    if actor.user_type == 'A' or not has_role(actor):
         return False
 
-    for role in ProjectDepartmentEmployeeRole.objects.filter(employee_id = actor.id):
+    for role in ProjectDepartmentEmployeeRole.objects.filter(employee_id=actor.id):
         if role.role_id.name == 'EMPLOYEE':
             return True
             
     return False
-       
     
 
 @register.assignment_tag
-def isProjectManager(actor):
-    if actor.user_type == 'A' or not hasRole(actor):
+def is_project_manager(actor):
+    if actor.user_type == 'A' or not has_role(actor):
         return False
 
-    for role in ProjectDepartmentEmployeeRole.objects.filter(employee_id = actor.id):
+    for role in ProjectDepartmentEmployeeRole.objects.filter(employee_id=actor.id):
         if role.role_id.name == 'PROJECT_MANAGER':
             return True
     
     return False
 
+
 @register.assignment_tag
-def isCoordinator(actor):
-    if actor.user_type == 'A' or not hasRole(actor):
+def is_coordinator(actor):
+    if actor.user_type == 'A' or not has_role(actor):
         return False
 
-    for role in ProjectDepartmentEmployeeRole.objects.filter(employee_id = actor.id):
+    for role in ProjectDepartmentEmployeeRole.objects.filter(employee_id=actor.id):
         if role.role_id.name == 'COORDINATOR':
             return True
 
@@ -104,22 +113,23 @@ def isCoordinator(actor):
 
 
 @register.assignment_tag
-def isExecutive(actor):
-    if actor.user_type == 'A' or not hasRole(actor):
+def is_executive(actor):
+    if actor.user_type == 'A' or not has_role(actor):
         return False
 
-    for role in ProjectDepartmentEmployeeRole.objects.filter(employee_id = actor.id):
+    for role in ProjectDepartmentEmployeeRole.objects.filter(employee_id=actor.id):
         if role.role_id.name == 'EXECUTIVE':
             return True
 
     return False
 
+
 @register.assignment_tag
-def isTeamManager(actor):
-    if actor.user_type == 'A' or not hasRole(actor):
+def is_team_manager(actor):
+    if actor.user_type == 'A' or not has_role(actor):
         return False
 
-    for role in ProjectDepartmentEmployeeRole.objects.filter(employee_id = actor.id):
+    for role in ProjectDepartmentEmployeeRole.objects.filter(employee_id=actor.id):
         if role.role_id.name == 'TEAM_MANAGER':
             return True
             
