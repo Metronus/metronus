728cc4b56bb2570fcbad5b82ac420351273e8b28
David de los Santos Boix <ddlsb@hotmail.com>
2017-06-28 18:42:30 +0200

    Ajax debería funcionar. Los puntos por comas también

--- Metronus-Project/metronus_app/templatetags/custom_tags.py
+++ Metronus-Project/metronus_app/templatetags/custom_tags.py
@@ -16,7 +16,7 @@ def get_form_type(form_type):
         'PasswordInput': 'password',
         'EmailInput': 'email',
         'ClearableFileInput': 'file',
-        'FloatField' : 'number'
+        'NumberInput' : 'numeric'
     }.get(form_type, 'text')
 
 
@@ -26,6 +26,7 @@ def show_form(form):
 
 @register.inclusion_tag('tags/field.html')
 def show_field(field, placeholder="", error_code=None):
+    print(get_type(field))
     return {'field': field, 'required': "*" if field.field.required else "",
             'type': get_form_type(get_type(field)), 'placeholder':placeholder,
             'custom_error_str':error_code}
