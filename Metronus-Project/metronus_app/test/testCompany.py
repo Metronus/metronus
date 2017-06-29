ff22477c2dce5120f906a2dea449e77f1ba2a9f0
anddonram <andrestp95@gmail.com>
2017-06-29 13:51:15 +0200

    tests de company register y edit

--- Metronus-Project/metronus_app/test/testCompany.py
+++ Metronus-Project/metronus_app/test/testCompany.py
@@ -9,8 +9,8 @@ import json
 
 class CompanyTestCase(TestCase):
     """This class provides a test case for companies signing up to the system"""
-
-    def setUp(self):
+    @classmethod
+    def setUpTestData(cls):
         """
         Loads the data to the database for tests to be done
         """
@@ -36,6 +36,37 @@ class CompanyTestCase(TestCase):
             phone="666555444",
             company_id=company1
         )
+        company2 = Company.objects.create(
+            cif="231232F",
+            company_name="Universidad de Sevilla2",
+            short_name="us2",
+            email="us2@gmail.com",
+            phone="123456789")
+
+        admin_user2 = User.objects.create_user(
+            username="admin2",
+            password="123456",
+            email="admin2@metronus.es",
+            first_name="Pepito",
+            last_name="Pérez"
+        )
+
+        Administrator.objects.create(
+            user=admin_user2,
+            user_type="A",
+            identifier="adm023",
+            phone="666555444",
+            company_id=company2
+        )
+    def test_register_company_get(self):
+        """Register new company form """
+        c = Client()
+        response = c.get(reverse("register"))
+    
+        self.assertEquals(response.status_code, 200)
+        form = response.context["form"]
+
+        self.assertFalse(form is None)
 
     def test_create_company(self):
         """
@@ -77,6 +108,267 @@ class CompanyTestCase(TestCase):
         logs_after = Company.objects.all().count()
 
         self.assertEquals(logs_before + 1, logs_after)
+    
+    def test_create_company_negative_1(self):
+        """
+        Register new company, agree_terms_error
+        """
+        c = Client()
+
+        response = c.post(reverse("register"), {
+            # Company
+            "cif" :"A01234567",
+            "company_name" :"comapniadejuego",
+            "short_name" :"cdj",
+            "company_email" :"emaildeemp@es.es",
+            "company_phone":"123456987",
+          
+
+            # User (Account data)
+            "username" : "usuarionumero1",
+            "admin_email" : "es@es.es",
+            "password" : "alcontraeslarga",
+            "repeatPassword" : "alcontraeslarga",
+            "first_name" :"pepito",
+            "last_name" : "name1",
+
+            # Administrator (Profile data)
+            "admin_identifier" :"ide",
+            "admin_phone" : "123456789",
+            "terms_agree" : "False",
+
+        })
+    
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('agree_terms_error' in response.context["errors"])   
+    def test_create_company_negative_2(self):
+        """
+        Register new company, company_short_name_duplicate
+        """
+        c = Client()
+
+        response = c.post(reverse("register"), {
+            # Company
+            "cif" :"A01234567",
+            "company_name" :"comapniadejuego",
+            "short_name" :"us",
+            "company_email" :"emaildeemp@es.es",
+            "company_phone":"123456987",
+          
+
+            # User (Account data)
+            "username" : "usuarionumero1",
+            "admin_email" : "es@es.es",
+            "password" : "alcontraeslarga",
+            "repeatPassword" : "alcontraeslarga",
+            "first_name" :"pepito",
+            "last_name" : "name1",
+
+            # Administrator (Profile data)
+            "admin_identifier" :"ide",
+            "admin_phone" : "123456789",
+            "terms_agree" : "True",
+
+        })
+    
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('company_short_name_duplicate' in response.context["errors"])   
+    def test_create_company_negative_3(self):
+        """
+        Register new company, companyRegister_cifNotUnique
+        """
+        c = Client()
+
+        response = c.post(reverse("register"), {
+            # Company
+            "cif" :"231231F",
+            "company_name" :"comapniadejuego",
+            "short_name" :"cdj",
+            "company_email" :"emaildeemp@es.es",
+            "company_phone":"123456987",
+          
+
+            # User (Account data)
+            "username" : "usuarionumero1",
+            "admin_email" : "es@es.es",
+            "password" : "alcontraeslarga",
+            "repeatPassword" : "alcontraeslarga",
+            "first_name" :"pepito",
+            "last_name" : "name1",
+
+            # Administrator (Profile data)
+            "admin_identifier" :"ide",
+            "admin_phone" : "123456789",
+            "terms_agree" : "True",
+
+        })
+    
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('companyRegister_cifNotUnique' in response.context["errors"])   
+    def test_create_company_negative_4(self):
+        """
+        Register new company, companyRegister_adminEmailNotUnique
+        """
+        c = Client()
+
+        response = c.post(reverse("register"), {
+            # Company
+            "cif" :"A01234567",
+            "company_name" :"comapniadejuego",
+            "short_name" :"cdj",
+            "company_email" :"emaildeemp@es.es",
+            "company_phone":"123456987",
+          
+
+            # User (Account data)
+            "username" : "usuarionumero1",
+            "admin_email" : "admin1@metronus.es",
+            "password" : "alcontraeslarga",
+            "repeatPassword" : "alcontraeslarga",
+            "first_name" :"pepito",
+            "last_name" : "name1",
+
+            # Administrator (Profile data)
+            "admin_identifier" :"ide",
+            "admin_phone" : "123456789",
+            "terms_agree" : "True",
+
+        })
+    
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('companyRegister_adminEmailNotUnique' in response.context["errors"])   
+    
+    def test_create_company_negative_5(self):
+        """
+        Register new company, companyRegister_companyEmailNotUnique
+        """
+        c = Client()
+
+        response = c.post(reverse("register"), {
+            # Company
+            "cif" :"A01234567",
+            "company_name" :"comapniadejuego",
+            "short_name" :"cdj",
+            "company_email" :"us@gmail.com",
+            "company_phone":"123456987",
+          
+
+            # User (Account data)
+            "username" : "usuarionumero1",
+            "admin_email" : "es@es.es",
+            "password" : "alcontraeslarga",
+            "repeatPassword" : "alcontraeslarga",
+            "first_name" :"pepito",
+            "last_name" : "name1",
+
+            # Administrator (Profile data)
+            "admin_identifier" :"ide",
+            "admin_phone" : "123456789",
+            "terms_agree" : "True",
+
+        })
+    
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('companyRegister_companyEmailNotUnique' in response.context["errors"])   
+    
+
+    def test_create_company_negative_6(self):
+        """
+        Register new company, newPasswordInvalid
+        """
+        c = Client()
+
+        response = c.post(reverse("register"), {
+            # Company
+            "cif" :"A01234567",
+            "company_name" :"comapniadejuego",
+            "short_name" :"cdj",
+            "company_email" :"emaildeemp@es.es",
+            "company_phone":"123456987",
+          
+
+            # User (Account data)
+            "username" : "usuarionumero1",
+            "admin_email" : "es@es.es",
+            "password" : "alcoa",
+            "repeatPassword" : "alcoa",
+            "first_name" :"pepito",
+            "last_name" : "name1",
+
+            # Administrator (Profile data)
+            "admin_identifier" :"ide",
+            "admin_phone" : "123456789",
+            "terms_agree" : "True",
+
+        })
+    
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('newPasswordInvalid' in response.context["errors"])   
+    def test_create_company_negative_7(self):
+        """
+        Register new company, companyRegister_usernameNotUnique
+        """
+        c = Client()
+
+        response = c.post(reverse("register"), {
+            # Company
+            "cif" :"A01234567",
+            "company_name" :"comapniadejuego",
+            "short_name" :"cdj",
+            "company_email" :"emaildeemp@es.es",
+            "company_phone":"123456987",
+          
+
+            # User (Account data)
+            "username" : "admin1",
+            "admin_email" : "es@es.es",
+            "password" : "alcontraeslarga",
+            "repeatPassword" : "alcontraeslarga",
+            "first_name" :"pepito",
+            "last_name" : "name1",
+
+            # Administrator (Profile data)
+            "admin_identifier" :"ide",
+            "admin_phone" : "123456789",
+            "terms_agree" : "True",
+
+        })
+    
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('companyRegister_usernameNotUnique' in response.context["errors"])   
+    def test_create_company_negative_8(self):
+        """
+        Register new company, passwordsDontMatch
+        """
+        c = Client()
+
+        response = c.post(reverse("register"), {
+            # Company
+            "cif" :"A01234567",
+            "company_name" :"comapniadejuego",
+            "short_name" :"cdj",
+            "company_email" :"emaildeemp@es.es",
+            "company_phone":"123456987",
+          
+
+            # User (Account data)
+            "username" : "usuarionumero1",
+            "admin_email" : "es@es.es",
+            "password" : "alcontraeslarga",
+            "repeatPassword" : "alcontraeslargae",
+            "first_name" :"pepito",
+            "last_name" : "name1",
+
+            # Administrator (Profile data)
+            "admin_identifier" :"ide",
+            "admin_phone" : "123456789",
+            "terms_agree" : "True",
+
+        })
+    
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('passwordsDontMatch' in response.context["errors"])   
+    
     def test_edit_company(self):
         """
         Edit  company
@@ -98,6 +390,35 @@ class CompanyTestCase(TestCase):
         self.assertEquals(Company.objects.get(cif="231231F").phone,"932833777")
         self.assertEquals(Company.objects.get(cif="231231F").email,"compania@pes.es")
         logs_after = Company.objects.all().count()
+    def test_edit_company_negative(self):
+        """
+        Edit  company, negative email
+        """
+        c = Client()
+        c.login(username="admin1", password="123456")
+        
+
+        response = c.post(reverse("company_edit"), {
+            # Company
+            'visible_short_name':True,
+            'company_email': "us2@gmail.com",
+            'company_phone': "932833777",
+        })
+    
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue('companyRegister_companyEmailNotUnique' in response.context["errors"])   
+
+    def test_edit_company_get(self):
+        """As an admin, try to get the edit company form """
+        c = Client()
+        c.login(username="admin1", password="123456")
+        response = c.get(reverse("company_edit"))
+    
+        self.assertEquals(response.status_code, 200)
+        form = response.context["form"]
+
+        self.assertEquals(form.initial["company_email"], "us@gmail.com")
+        self.assertEquals(form.initial["company_phone"], "123456789")
 
 
     def test_shortname_positive(self):
