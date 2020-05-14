c6bca6af55043834af707148c00d6e14228dbf67
David de los Santos Boix <Dionakra@users.noreply.github.com>
2020-05-14 11:25:24 +0100

    Update settings.py

--- Metronus-Project/metronus/settings.py
+++ Metronus-Project/metronus/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not production
 
-ALLOWED_HOSTS = [".metronus.es", "metronus.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = [".metronus.es", "metronus.herokuapp.com", "localhost", "metronusapp.herokuapp.com"]
 
 LANGUAGES = (
     ('en', _('English')),
